c8a1055ff11815e6f03d1c6adfe58da29ba02f02
kahassanzadeh <kamyar.hassanzadeh.78@gmail.com>
2023-01-27 23:09:12 +0330

    input output fixed

--- Parse.py
+++ Parse.py
@@ -1,7 +1,6 @@
 class ParseObj:
-    def __int__(self, ifstate=False):
+    def __int__(self):
         self.code = ''
         self.ids = []
         self.value = ''
         self.type = ''
-        self.ifstate = ifstate
